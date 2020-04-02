6250d1229b631b11a1533d64bdce3b6c50ca45df
fakeboom <fakeboom@foxmail.com>
2020-04-02 14:16:47 +0800

    init

---
+++ client.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"net/http"
+)
+
+func httpGet() {
+	resp, err := http.Get("http://localhost:3000/hello")
+	if err != nil {
+		// handle error
+	}
+
+	defer resp.Body.Close()
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		// handle error
+	}
+
+	fmt.Println(string(body))
+}
+func main() {
+	httpGet()
+}
